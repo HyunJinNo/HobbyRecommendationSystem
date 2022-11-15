c4de45f98979c601e547eb12230d8639ef9af1e5
HyunJinNo <nhj9831@naver.com>
2022-11-15 14:39:36 +0900

    로그인, 회원가입 기능 구현 완료

--- app/src/main/java/com/hyunjin/hobbyrecommendationsystem/MainActivity.kt
+++ app/src/main/java/com/hyunjin/hobbyrecommendationsystem/MainActivity.kt
@@ -3,106 +3,57 @@ package com.hyunjin.hobbyrecommendationsystem
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.hyunjin.hobbyrecommendationsystem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var auth: FirebaseAuth
+    private val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Initialize Firebase Auth
-        auth = Firebase.auth
-
         binding.LogInButton.setOnClickListener {
-            // TODO
+            val id = binding.LogInIDEditText.text.toString()
+            val password = binding.LogInPasswordEditText.text.toString()
+
+            if (id.isEmpty()) {
+                Toast.makeText(applicationContext, "Your ID is empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                Toast.makeText(applicationContext, "Your password is empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            db.collection("Accounts").document(id)
+                .get()
+                .addOnSuccessListener { document ->
+                    if (document.exists()) {
+                        val data = document.data!!["password"]
+                        if (password == data) {
+                            Toast.makeText(applicationContext, "Succeeded in logging in", Toast.LENGTH_SHORT).show()
+                            // TODO
+                            // StartActivity(Intent(this, ?))
+                        } else {
+                            Toast.makeText(applicationContext, "Your password is not correct", Toast.LENGTH_SHORT).show()
+                        }
+                    } else {
+                        Toast.makeText(applicationContext, "Your ID is not present", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .addOnFailureListener {
+                    Toast.makeText(applicationContext, "Failed in logging in", Toast.LENGTH_SHORT).show()
+                }
         }
 
         binding.SignUpButton.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
         }
-    }
-
-    public override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        if (currentUser != null) {
-            reload();
-        }
-    }
-
-    private fun createAccount(email: String, password: String) {
-        // [START create_user_with_email]
-
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) {
-                if (it.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithEmail:failure", it.exception)
-                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
-                    updateUI(null)
-                }
-            }
-
-        // [END create_user_with_email]
-    }
-
-    private fun signIn(email: String, password: String) {
-        // [START sign_in_with_email]
-
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) {
-                if (it.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithEmail:success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithEmail:failure", it.exception)
-                    Toast.makeText(baseContext, "Authentication failed", Toast.LENGTH_SHORT).show()
-                    updateUI(null)
-                }
-            }
-
-        // [END sign-in_with_email]
-    }
-
-    private fun sendEmailVerification() {
-        // [START send_email_verification]
-
-        val user = auth.currentUser!!
-        user.sendEmailVerification().addOnCompleteListener(this) {
-            // Email Verification sent
-        }
-
-        // [END sign_in_with_email]
-    }
-
-    private fun updateUI(user: FirebaseUser?) {
-        // TODO
-    }
-
-    private fun reload() {
-        // TODO
-    }
-
-    companion object {
-        private const val TAG = "EmailPassword"
     }
 }
--- app/src/main/java/com/hyunjin/hobbyrecommendationsystem/SignUpActivity.kt
+++ app/src/main/java/com/hyunjin/hobbyrecommendationsystem/SignUpActivity.kt
@@ -2,14 +2,90 @@ package com.hyunjin.hobbyrecommendationsystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.hyunjin.hobbyrecommendationsystem.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    
+    // Access a Cloud Firestore instance from your Activity
+    private val db = Firebase.firestore
+    private var isAvailableID = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.CheckAvailabilityButon.setOnClickListener {
+            val id = binding.SignUpIDEditText.text.toString()
+
+            if (id.isEmpty()) {
+                Toast.makeText(applicationContext, "Your ID is empty", Toast.LENGTH_SHORT).show()
+            } else {
+                db.collection("Accounts").document(id)
+                    .get()
+                    .addOnSuccessListener { document ->
+                        isAvailableID = if (document.exists()) {
+                            Toast.makeText(applicationContext, "Your ID is already present", Toast.LENGTH_SHORT).show()
+                            binding.SignUpIDEditText.text.clear()
+                            false
+                        } else {
+                            Toast.makeText(applicationContext, "Your ID is available", Toast.LENGTH_SHORT).show()
+                            binding.SignUpIDEditText.isFocusable = false
+                            true
+                        }
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(applicationContext, "Failed in checking availability", Toast.LENGTH_SHORT).show()
+                    }
+            }
+        }
+
+        binding.SignUpButton2.setOnClickListener {
+            val id = binding.SignUpIDEditText.text.toString()
+            val password = binding.SignUpPasswordEditText.text.toString()
+            val confirmation = binding.SignUpConfirmationEditText.text.toString()
+            
+            if (id.isEmpty()) {
+                Toast.makeText(applicationContext, "Your ID is empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else if (!isAvailableID) {
+                Toast.makeText(applicationContext, "Please check if your ID is available", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                Toast.makeText(applicationContext, "Your password is empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (confirmation.isEmpty()) {
+                Toast.makeText(applicationContext, "Please enter a password for confirmation", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else if (password != confirmation) {
+                Toast.makeText(applicationContext, "Failed to confirm your password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            // Create a new user with password
+            val user = hashMapOf(
+                "password" to password
+            )
+
+            // Add a new document
+            db.collection("Accounts").document(id)
+                .set(user)
+                .addOnSuccessListener {
+                    Toast.makeText(applicationContext, "Succeeded in making a new account", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(applicationContext, "Failed in making a new account", Toast.LENGTH_SHORT).show()
+                    return@addOnFailureListener
+                }
+        }
     }
 }
