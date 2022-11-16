c9c693e46e808c26f17bb1cc06ba30c02a87e389
HyunJinNo <nhj9831@naver.com>
2022-11-16 10:18:47 +0900

    Add MainActivity.kt

---
+++ app/src/main/java/com/hyunjin/hobbyrecommendationsystem/LogInActivity.kt
@@ -0,0 +1,58 @@
+package com.hyunjin.hobbyrecommendationsystem
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.hyunjin.hobbyrecommendationsystem.databinding.ActivityLogInBinding
+
+class LogInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLogInBinding
+    private val db = Firebase.firestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.LogInButton.setOnClickListener {
+            val id = binding.LogInIDEditText.text.toString()
+            val password = binding.LogInPasswordEditText.text.toString()
+
+            if (id.isEmpty()) {
+                Toast.makeText(applicationContext, "Your ID is empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                Toast.makeText(applicationContext, "Your password is empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            db.collection("Accounts").document(id)
+                .get()
+                .addOnSuccessListener { document ->
+                    if (document.exists()) {
+                        val data = document.data!!["password"]
+                        if (password == data) {
+                            Toast.makeText(applicationContext, "Succeeded in logging in", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this, MainActivity::class.java))
+                        } else {
+                            Toast.makeText(applicationContext, "Your password is not correct", Toast.LENGTH_SHORT).show()
+                        }
+                    } else {
+                        Toast.makeText(applicationContext, "Your ID is not present", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .addOnFailureListener {
+                    Toast.makeText(applicationContext, "Failed in logging in", Toast.LENGTH_SHORT).show()
+                }
+        }
+
+        binding.SignUpButton.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+    }
+}
--- app/src/main/java/com/hyunjin/hobbyrecommendationsystem/MainActivity.kt
+++ app/src/main/java/com/hyunjin/hobbyrecommendationsystem/MainActivity.kt
@@ -1,59 +1,30 @@
 package com.hyunjin.hobbyrecommendationsystem
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import android.view.Menu
+import android.view.MenuItem
 import com.hyunjin.hobbyrecommendationsystem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
 
-        binding.LogInButton.setOnClickListener {
-            val id = binding.LogInIDEditText.text.toString()
-            val password = binding.LogInPasswordEditText.text.toString()
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
 
-            if (id.isEmpty()) {
-                Toast.makeText(applicationContext, "Your ID is empty", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-
-            if (password.isEmpty()) {
-                Toast.makeText(applicationContext, "Your password is empty", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-
-            db.collection("Accounts").document(id)
-                .get()
-                .addOnSuccessListener { document ->
-                    if (document.exists()) {
-                        val data = document.data!!["password"]
-                        if (password == data) {
-                            Toast.makeText(applicationContext, "Succeeded in logging in", Toast.LENGTH_SHORT).show()
-                            // TODO
-                            // StartActivity(Intent(this, ?))
-                        } else {
-                            Toast.makeText(applicationContext, "Your password is not correct", Toast.LENGTH_SHORT).show()
-                        }
-                    } else {
-                        Toast.makeText(applicationContext, "Your ID is not present", Toast.LENGTH_SHORT).show()
-                    }
-                }
-                .addOnFailureListener {
-                    Toast.makeText(applicationContext, "Failed in logging in", Toast.LENGTH_SHORT).show()
-                }
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            // TODO: settingsFragment
+            true
         }
-
-        binding.SignUpButton.setOnClickListener {
-            startActivity(Intent(this, SignUpActivity::class.java))
-        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
